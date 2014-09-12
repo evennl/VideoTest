65454b7677a97d3f9be2a03f3a6e7dbd7373b54f
evennl <ljwqq0@gmail.com>
2014-09-12 16:38:50 +0900

    Test code. Can't run.

--- VideoTest/src/edu/u_tokyo/kmjlab/liu/videotest/Main.java
+++ VideoTest/src/edu/u_tokyo/kmjlab/liu/videotest/Main.java
@@ -1,17 +1,69 @@
 package edu.u_tokyo.kmjlab.liu.videotest;
 
+import org.bytedeco.javacpp.opencv_core.IplImage;
+import org.bytedeco.javacv.FFmpegFrameGrabber;
+import org.bytedeco.javacv.FFmpegFrameRecorder;
+import org.bytedeco.javacv.FrameGrabber;
+import org.bytedeco.javacv.FrameGrabber.Exception;
+
 public class Main
 {
 	public static void main(String[] args)
 	{
 		System.out.println("Hello world!");
-		for(int i = 0; i < 10; i++)
+
+
+		FrameGrabber grabber = new FFmpegFrameGrabber("D:/person01_handwaving_d1_uncomp.avi");
+		try
 		{
-			System.out.println(i);
+			grabber.start();
 		}
-		for(int i = 0; i < 10; i++)
+		catch (Exception e)
 		{
-			System.out.println(i);
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+
+		final int frameRate = 30;
+		int frameWidth = 640;
+		int frameHeight = 480;
+
+		FFmpegFrameRecorder recorder = new FFmpegFrameRecorder("D:/2.mp4", frameWidth, frameHeight, 0);
+		recorder.setFormat("mp4");
+		recorder.setFrameRate(frameRate);
+
+		try
+		{
+			recorder.start();
+		}
+		catch (org.bytedeco.javacv.FrameRecorder.Exception e)
+		{
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		IplImage frame = null;
+		int i = 1;
+		while (true)
+		{
+			System.out.println(i++);
+			try
+			{
+				frame = grabber.grab();
+				if(frame == null)
+				{
+					System.out.println("End of Video");
+					break;
+				}
+				recorder.record(frame);
+			}
+			catch (Exception | org.bytedeco.javacv.FrameRecorder.Exception e)
+			{
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			
 		}
 	}
 }
